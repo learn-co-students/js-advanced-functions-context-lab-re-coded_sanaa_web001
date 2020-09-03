23ab4b64aae6799ade001a5b9c767b2f6b9504a3
Nusiba Ibrahim <ainusiba@gmail.com>
2020-09-03 09:46:13 +0000

    Done.

--- index.js
+++ index.js
@@ -20,3 +20,64 @@ let allWagesFor = function () {
 
     return payable
 }
+
+function createEmployeeRecord(employeeInfo){
+    let record = {}
+    record.firstName = employeeInfo[0]
+    record.familyName = employeeInfo[1]
+    record.title = employeeInfo[2]
+    record.payPerHour = employeeInfo[3]
+    record.timeInEvents = []
+    record.timeOutEvents = []
+
+    return record
+}
+
+function createEmployeeRecords(employeesArray){
+    return employeesArray.map(employee => createEmployeeRecord(employee))
+}
+
+function createTimeInEvent(dateStamp){ //no employee Record anymore bc of "this"
+    let timeInObj = {}
+    timeInObj.type = "TimeIn"
+    timeInObj.hour = parseInt(dateStamp.split(" ")[1])
+    timeInObj.date = dateStamp.split(" ")[0]
+
+    this.timeInEvents.push(timeInObj)
+    return this
+}
+
+function createTimeOutEvent(dateStamp){
+    let timeOutObj = {}
+    timeOutObj.type = "TimeOut"
+    timeOutObj.hour = parseInt(dateStamp.split(" ")[1])
+    timeOutObj.date = dateStamp.split(" ")[0]
+
+    this.timeOutEvents.push(timeOutObj)
+    return this
+
+}
+
+function hoursWorkedOnDate(dateStamp){
+    //find the time in (hour) for date
+    const timeInForDate = this.timeInEvents.find(e => e.date === dateStamp)
+    const timeOutForDate = this.timeOutEvents.find(e => e.date === dateStamp)
+
+    return ((timeOutForDate.hour - timeInForDate.hour)/100)
+
+}
+
+function wagesEarnedOnDate(dateStamp){
+
+    return hoursWorkedOnDate.call(this, dateStamp) * this.payPerHour
+
+}
+
+function findEmployeeByFirstName(employees, firstName){
+   return employees.find(employee => employee.firstName === firstName)
+}
+
+function calculatePayroll(allEmployees){
+    return allEmployees.reduce((total, employee) => total + allWagesFor.call(employee), 0) 
+
+} 
